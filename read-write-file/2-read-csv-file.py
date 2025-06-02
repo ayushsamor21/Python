cbf60b0328aa37e60e068cb3a80ec926fe4e503f
ayushsamor21 <ayushsamor21@gmail.com>
2025-06-02 18:56:39 +0530

    Read csv file using python

--- read-write-file/2-read-csv-file.py
+++ read-write-file/2-read-csv-file.py
@@ -3,7 +3,7 @@ import csv
 def read_file(file_name):
     with open(file_name, mode='r') as file:
         reader = csv.DictReader(file)
-        print(reader)   #<csv.DictReader object at 0x1008b6f90>
+        print(reader)   #<csv.DictReader object at "memory_location">
         for row in reader:
             print("Name:", row["NAME"])
             print("State:", row["STATE"])
